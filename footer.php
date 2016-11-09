74df1165e4f7f92df1e70f5760ed62521cbe475b
jaamiem <jamiemuir1994@gmail.com>
2016-11-09 12:42:34 +0000

    added footer html(in php) file

---
+++ footer.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		<div class="footer">
+
+			<div class="colLeft">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">News</a></li>
+				<li><a href="#">Student Support</a>
+					<ul>
+						<li><a href="#">Disability</a></li>
+						<li><a href="#">Wellbeing</a></li>
+						<li><a href="#">Study Support</a></li>
+						<li><a href="#">Careers</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Contact Us</a></li>
+			</div>	
+	
+			<div class="colRight">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2166.09035040788!2d-2.143216148306454!3d57.11841899321549!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x488410483d2f0941%3A0x5d8969688e77366a!2sRobert+Gordon+University+Aberdeen%2C+Garthdee+Campus!5e0!3m2!1sen!2suk!4v1478692046430" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+			</div>
+
+			<div class="colCentre">
+				<li><a href="#">Courses</a>
+					<ul>
+						<li><a href="#">Undergraduate</a>
+							<ul>
+								<li><a href="#">COURSE1</a></li>
+								<li><a href="#">COURSE2</a></li>
+								<li><a href="#">COURSE3</a></li>
+							</ul>
+						</li>
+						<li><a href="#">Postgraduate</a>
+							<ul>
+								<li><a href="#">COURSE1</a></li>
+								<li><a href="#">COURSE2</a></li>
+								<li><a href="#">COURSE3</a></li>
+							</ul>
+						</li>
+					</ul>
+				</li>
+			</div>
+
+		</div>
+	</body>
+
+</html>
