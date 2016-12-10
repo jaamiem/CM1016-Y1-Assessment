f0be850ed3cc5ce5dc22c153778b8098c9782ee3
teodor20 <teodor.narliyski@gmail.com>
2016-12-10 22:57:32 +0000

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="styles.css" type="text/css">
+	<title>RGU CSDM</title>
+</head>
+<body>
+	<div class="wrapper">
+		<div id="header">
+			<img id="header" src="rgulogo.gif" alt="RGU Logo Header"></img>
+			<div id="nav">
+				<nav>
+					<ul>
+						<li><a href="#">Home</a></li>
+						<li><a href="#">News</a></li>
+						<li><a href="#">Student Support</a>
+							<ul>
+								<li><a href="#">Disability</a></li>
+								<li><a href="#">Wellbeing</a></li>
+								<li><a href="#">Study Support</a></li>
+								<li><a href="#">Careers</a></li>
+							</ul>
+						</li>
+						<li><a href="#">Contact Us</a></li>
+						<li><a href="#">Courses</a>
+							<ul>
+								<li><a href="#">Undergraduate</a>
+									<ul>
+										<li><a href="#">COURSE1</a></li>
+										<li><a href="#">COURSE2</a></li>
+										<li><a href="#">COURSE3</a></li>
+									</ul>
+								</li>
+								<li><a href="#">Postgraduate</a>
+									<ul>
+										<li><a href="#">COURSE1</a></li>
+										<li><a href="#">COURSE2</a></li>
+										<li><a href="#">COURSE3</a></li>
+									</ul>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+		<div id="content">
+			<div id="content">
+			<h2 style="padding-left:7.5%;"> Contact Us </h2>
+			<hr></hr>
+			<div class="CenterPanel">
+      <div class="leftpanel"><b>Address
+			<i><p>Robert Gordon University</p>
+<p>Garthdee House</p>
+<p>Garthdee Road</p>
+<p>Aberdeen
+<p>AB10 7QB</p></i></b>
+			</div>
+			<div class="rightpanel"><b>Phone Numbers
+			<i><p>The main telephone number for the University is: 01224 262000 when dialling within the UK</p> 
+			<p>+44 1224 262000 when dialling internationally</p></i></b>
+			</div>
+			</div>
+            <div class="contactMap">
+				 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4332.181050899108!2d-2.141022!3d57.118416!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5d8969688e77366a!2sRobert+Gordon+University+Aberdeen%2C+Garthdee+Campus!5e0!3m2!1sen!2suk!4v1479388108545" width="400" height="220" frameborder="0" style="border:0" allowfullscreen></iframe>
+		
+			
+			</div>
+			
+			<div class="contentRight">
+				<a class="twitter-timeline" data-width="300" href="https://twitter.com/RGUComputing">Tweets by RGUComputing</a> 
+				<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+			</div>
+		</div>
+		<div id="footer">
+		<footer>
+			<div class="leftClusterContacts">
+				<h2> School of Computing and Digital Media </h2>
+				<div class="colLeftContacts">
+					<li><a href="#">Home</a></li>
+					<li><a href="#">News</a></li>
+					<li><a href="#">Student Support</a>
+						<ul>
+							<li><a href="#">Disability</a></li>
+							<li><a href="#">Wellbeing</a></li>
+							<li><a href="#">Study Support</a></li>
+							<li><a href="#">Careers</a></li>
+						</ul>
+					</li>
+					<li><a href="#">Contact Us</a></li>
+				</div>	
+
+				<div class="colCentreContacts">
+					<li><a href="#">Courses</a>
+						<ul>
+							<li><a href="#">Undergraduate</a>
+								<ul>
+									<li><a href="#">COURSE1</a></li>
+									<li><a href="#">COURSE2</a></li>
+									<li><a href="#">COURSE3</a></li>
+								</ul>
+							</li>
+							<li><a href="#">Postgraduate</a>
+								<ul>
+									<li><a href="#">COURSE1</a></li>
+									<li><a href="#">COURSE2</a></li>
+									<li><a href="#">COURSE3</a></li>
+								</ul>
+							</li>
+						</ul>
+					</li>
+				</div>
+			</div>
+
+		</footer>
+		</div>
+		<p>@2016 copyright Harambe inc. your soul now belongs to Donald Trump. We know where you live. </p>
+	</div>
+</body>
+</html>
