eb4c9d3a4f5767714bccab9c970468a18ab31de6
teodor20 <teodor.narliyski@gmail.com>
2016-12-13 16:19:16 +0000

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,121 +1,560 @@
 <!DOCTYPE html>
-<html>
+
+<html lang="en">
+
 <head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" href="styles.css" type="text/css">
-	<title>RGU CSDM</title>
+
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+
+
+    <title>North Lab</title>
+
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+
+    <link rel="stylesheet" href="main page.css">
+	    <link rel="stylesheet" href="bootstrap.css">
+	    <link rel="stylesheet" href="ripple.css">
+		<link rel="stylesheet" href="contact.css">
+
+
+
+
+
+
+
+
+    <link href="bootstrap-material-design.css" rel="stylesheet">
+
+    <link href="ripples.css" rel="stylesheet">
+
+
+
+
+
+
+
 </head>
+
 <body>
-	<div class="wrapper">
-		<div id="header">
-			<img id="header" src="rgulogo.gif" alt="RGU Logo Header"></img>
-			<div id="nav">
-				<nav>
-					<ul>
-						<li><a href="#">Home</a></li>
-						<li><a href="#">News</a></li>
-						<li><a href="#">Student Support</a>
-							<ul>
-								<li><a href="#">Disability</a></li>
-								<li><a href="#">Wellbeing</a></li>
-								<li><a href="#">Study Support</a></li>
-								<li><a href="#">Careers</a></li>
-							</ul>
-						</li>
-						<li><a href="#">Contact Us</a></li>
-						<li><a href="#">Courses</a>
-							<ul>
-								<li><a href="#">Undergraduate</a>
-									<ul>
-										<li><a href="#">COURSE1</a></li>
-										<li><a href="#">COURSE2</a></li>
-										<li><a href="#">COURSE3</a></li>
-									</ul>
-								</li>
-								<li><a href="#">Postgraduate</a>
-									<ul>
-										<li><a href="#">COURSE1</a></li>
-										<li><a href="#">COURSE2</a></li>
-										<li><a href="#">COURSE3</a></li>
-									</ul>
-								</li>
-							</ul>
-						</li>
-					</ul>
-				</nav>
-			</div>
-		</div>
-		<div id="content">
-			<div id="content">
-			<h2 style="padding-left:7.5%;"> Contact Us </h2>
-			<hr></hr>
-			<div class="CenterPanel">
-      <div class="leftpanel"><b>Address
-			<i><p>Robert Gordon University</p>
-<p>Garthdee House</p>
+
+<!-Header starts here -->
+
+
+
+
+
+    <!--Main content start here -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class = "container">
+
+
+
+
+
+    <header class="col-md-12">
+
+        <h1>Robert Gordan University</h1>
+
+
+
+        <h2>School of Computing Science and Digital Media</h2>
+
+    </header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <nav class="col-md-12 navbar navbar-default navbar-primary">
+
+            <div class="container-fluid content-width ">
+
+
+
+                <div class =  "navbar-header">
+
+
+
+                    <button class="btn-block navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-top" aria-expanded="false">
+
+                        <span class="sr-only">Toggle</span>
+
+                        Main Menu
+
+                        <span class="icon">
+
+                            <span class="icon-bar"></span>
+
+                            <span class="icon-bar"></span>
+
+                            <span class="icon-bar"></span>
+
+                        </span>
+
+
+
+
+
+
+
+
+
+                    </button>
+
+                    </div>
+
+
+
+                <div id = "navbar-collapse-top" class = "collapse navbar-collapse">
+
+                    <ul class=" nav navbar-nav">
+
+                        <li class = "active">
+
+                        <a class="text-upperscase" href="/">
+
+                            Home
+
+                            <span class = "sr-only">{current)</span>
+
+                            </a>
+
+                        </li>
+
+
+
+                        <li class = "">
+
+                        <a class = "text-uppercase" href="/">
+
+                            News
+
+                            </a>
+
+                        </li>
+
+
+
+                        <li class = "">
+
+                            <a class = "text-uppercase" href="/">
+
+                                Courses
+
+                            </a>
+
+                        </li>
+
+
+
+                        <li class = "">
+
+                            <a class = "text-uppercase" href="/">
+
+                                About us
+
+                            </a>
+
+
+
+                            <span class=" visible-md"></span>
+
+                        </li>
+
+                        </ul>
+
+                    </div>
+
+                </div>
+
+            </nav>
+
+
+
+
+
+<!--Header Finishes here -->
+
+<!--Container starts here-->
+<!--FIRST ROW-->
+
+
+<main class="col-md-12">
+
+        <div id="experimentContainer" class="row">
+
+
+
+
+
+            <div class="col-md-4">
+
+                <div class="panel panel-default">
+
+
+                    <div class="panel-body">
+
+    <h4><a> General Enquires  </a></h4>
+
+                        <p>School Office</p>
+<p>School of Computing Science and Digital Media</p>
+<p>The Robert Gordon University</p>
+<p>Sir Ian Wood Building</p>
 <p>Garthdee Road</p>
-<p>Aberdeen
-<p>AB10 7QB</p></i></b>
-			</div>
-			<div class="rightpanel"><b>Phone Numbers
-			<i><p>The main telephone number for the University is: 01224 262000 when dialling within the UK</p> 
-			<p>+44 1224 262000 when dialling internationally</p></i></b>
-			</div>
-			</div>
-            <div class="contactMap">
-				 <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4332.181050899108!2d-2.141022!3d57.118416!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5d8969688e77366a!2sRobert+Gordon+University+Aberdeen%2C+Garthdee+Campus!5e0!3m2!1sen!2suk!4v1479388108545" width="400" height="220" frameborder="0" style="border:0" allowfullscreen></iframe>
+<p>Aberdeen</p>
+<p>AB10 7GJ</p>
+<p>Email: csdm-office@rgu.ac.uk</p>
+<p>Tel: 44-(0)1224-262700</p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+
+
+
+
+
+
+           <div class="col-md-4">
+
+                <div class="panel panel-default">
+
+
+                    <div class="panel-body">
+
+    <h4><a> Undergraduate Enquires  </a></h4>
+
+                        <p>School Office</p>
+<p>School of Computing Science and Digital Media</p>
+<p>The Robert Gordon University</p>
+<p>Sir Ian Wood Building</p>
+<p>Garthdee Road</p>
+<p>Aberdeen</p>
+<p>AB10 7GJ</p>
+<p>Email: csdm-office@rgu.ac.uk</p>
+<p>Tel: 44-(0)1224-262700</p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+        <div class="col-md-4">
+
+            <div class="panel panel-primary">
+
+                <div class="panel-heading">
+
+
+
+                    <h3>Quick Links</h3>
+
+                    </div>
+
+                    <div class="panel-body">
+
+                        <ul><a href="https://campusmoodle.rgu.ac.uk/login/index.php">Campus Moodle</a></ul>
+
+                        <ul><a href="https://you.rgu.ac.uk/">RGYOU</a></ul>
+
+                        <ul><a href="http://www.rgu.ac.uk/">RGU Home</a></ul>
+
+                        <ul><a href="#">Computing Facebook</a></ul>
+
+
+
+
+
+                    </div>
+					
 		
-			
-			</div>
-			
-			<div class="contentRight">
-				<a class="twitter-timeline" data-width="300" href="https://twitter.com/RGUComputing">Tweets by RGUComputing</a> 
-				<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-			</div>
-		</div>
-		<div id="footer">
-		<footer>
-			<div class="leftClusterContacts">
-				<h2> School of Computing and Digital Media </h2>
-				<div class="colLeftContacts">
-					<li><a href="#">Home</a></li>
-					<li><a href="#">News</a></li>
-					<li><a href="#">Student Support</a>
-						<ul>
-							<li><a href="#">Disability</a></li>
-							<li><a href="#">Wellbeing</a></li>
-							<li><a href="#">Study Support</a></li>
-							<li><a href="#">Careers</a></li>
-						</ul>
-					</li>
-					<li><a href="#">Contact Us</a></li>
-				</div>	
 
-				<div class="colCentreContacts">
-					<li><a href="#">Courses</a>
-						<ul>
-							<li><a href="#">Undergraduate</a>
-								<ul>
-									<li><a href="#">COURSE1</a></li>
-									<li><a href="#">COURSE2</a></li>
-									<li><a href="#">COURSE3</a></li>
-								</ul>
-							</li>
-							<li><a href="#">Postgraduate</a>
-								<ul>
-									<li><a href="#">COURSE1</a></li>
-									<li><a href="#">COURSE2</a></li>
-									<li><a href="#">COURSE3</a></li>
-								</ul>
-							</li>
-						</ul>
-					</li>
-				</div>
-			</div>
+                </div>
+				
+				
 
-		</footer>
-		</div>
-		<p>@2016 copyright Harambe inc. your soul now belongs to Donald Trump. We know where you live. </p>
-	</div>
+            </div>
+			
+			<!--END OF FIRST ROW-->
+			
+			
+			<!--START OF SECOND ROW-->
+			
+			
+			
+	<main class="col-md-12">
+
+        <div id="experimentContainer" class="row">
+			
+			 <div class="col-md-4">
+
+                <div class="panel panel-default">
+
+
+                    <div class="panel-body">
+
+    <h4><a> Postgarduate Enquires  </a></h4>
+
+                        <p>School Office</p>
+<p>School of Computing Science and Digital Media</p>
+<p>The Robert Gordon University</p>
+<p>Sir Ian Wood Building</p>
+<p>Garthdee Road</p>
+<p>Aberdeen</p>
+<p>AB10 7GJ</p>
+<p>Email: csdm-office@rgu.ac.uk</p>
+<p>Tel: 44-(0)1224-262700</p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+
+
+
+
+
+
+           <div class="col-md-4">
+
+                <div class="panel panel-default">
+
+
+                    <div class="panel-body">
+
+    <h4><a> Research Enquires  </a></h4>
+
+                        <p>School Office</p>
+<p>School of Computing Science and Digital Media</p>
+<p>The Robert Gordon University</p>
+<p>Sir Ian Wood Building</p>
+<p>Garthdee Road</p>
+<p>Aberdeen</p>
+<p>AB10 7GJ</p>
+<p>Email: csdm-office@rgu.ac.uk</p>
+<p>Tel: 44-(0)1224-262700</p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+			
+			<div class="col-md-4">
+
+                <div class="panel panel-default">
+
+
+
+                    <div class="panel-heading">
+
+                    <div class="header-col">
+
+                    </div>
+
+                    </div>
+
+                    <div class="panel-body">
+
+                        <a class="twitter-timeline" data-width="300" data-height="210" data-dnt="true" href="https://twitter.com/RGUComputing">Tweets by RGUComputing</a>
+
+                        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+						
+
+
+
+
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+
+
+
+
+
+			
+</div>
+<!--End of SECOND ROW-->
+
+<!--Start of THIRD ROW-->
+
+<div id="experimentContainer" class="row">
+			
+			 <div class="col-md-4">
+
+                <div class="panel panel-default">
+
+
+                    <div class="panel-body">
+
+    <h4><a> Consultanices  </a></h4>
+
+                        <p>Attn: Mrs Alison Orellana</p>
+<p>School of Computing Science and Digital Media</p>
+<p>The Robert Gordon University</p>
+<p>Sir Ian Wood Building</p>
+<p>Garthdee Road</p>
+<p>Aberdeen</p>
+<p>AB10 7GJ</p>
+<p>Email: csdm-office@rgu.ac.uk</p>
+<p>Tel: 44-(0)1224-262700</p>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+
+
+
+
+
+
+           <div class="col-md-4">
+
+                <div class="panel panel-default">
+
+
+                    <div class="panel-body">
+
+    <h4><a> International Office  </a></h4>
+
+                        <p>To get in contact with the International office</p> 
+						<p>please follow this link:</p>
+						<a href="http://www.rgu.ac.uk/about/faculties-schools-and-departments/administration-departments/international-office">International</a>
+
+
+
+                    </div>
+
+                </div>
+
+            </div>
+
+			<!-- End of Third ROW -->
+<!--End of container-->
+</main>
+
+<!--Start of footer-->
+
+
+
+
+
+
+
+    <footer class="col-md-12">
+
+
+
+        <section class="col-md-4">
+
+            <u><h3>School of Computing Science and digital media</h3></u>
+
+
+
+            <p>Germanus, gratis seculas virtualiter demitto de emeritis, superbus devatio.Abactuss cadunt in emeritis
+
+                hamburgum!</p>
+
+
+
+            <div class = "colRight">
+
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d4332.181050899108!2d-2.141022!3d57.118416!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x5d8969688e77366a!2sRobert+Gordon+University+Aberdeen%2C+Garthdee+Campus!5e0!3m2!1sen!2suk!4v1479388108545" width="400" height="220" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+                <h3> Robert Gordon University<br> Sir Ian Wood Building<br> Garthdee Road<br> Aberdeen, AB10 7GJ</h3>
+
+            </div>
+
+
+
+        </section>
+
+        </div>
+
+
+
+
+
+    </footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--CONTENT ENDS HERE-->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js"></script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 </body>
+
 </html>
