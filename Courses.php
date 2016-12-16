ec14344a57cfbb4d6c279c64312889a215d8fbc5
Jamie Muir <jaamiem@users.noreply.github.com>
2016-12-16 16:25:09 +0000

    Add files via upload

---
+++ Courses.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include_once("head.php"); ?>
+
+<body>
+<!-Header starts here -->
+	<!--Main content start here -->
+	<div class = "container">
+		<div id="header">
+			<?php include_once("header.php"); ?>
+		</div>
+
+	<!--Header Finishes here -->
+	<!-- Code for Container start here-->
+	<main class="col-md-12">
+		<h3> Undergraduate </h3>
+		Computer Science:<br>
+			Learn object- orintated software desing and development skills, that will give you the foundation 
+			to be a professional software designer who can use different enviroments and product development 
+			technologies.
+			For more information <a href="http://www.rgu.ac.uk/computing/study-options/undergraduate/computer-science">click here</a>
+		<br><br>
+		Computing - Software Technology with Network Management:<br>
+			Two-year  course designed for aoolicants who completed a relevantt HND in Computing. This course
+			is a top-up progrgam which leads to an Honours degree.
+			For more information<a href="http://www.rgu.ac.uk/computing/study-options/postgraduate-taught-full-time/computing-software-technology-with-network-management"> click here</a>
+		<br><br>
+		Computer Network Managment and Design:<br>
+			Course which concentrates on developing the personal and transferable skills needed to
+			develop mangage computer networks and receive comprehensive Cisco CCNA training.
+			For more information <a href="http://www.rgu.ac.uk/computing/study-options/undergraduate/computer-network-management-and-design">click here</a>
+
+
+		<h3> Postgraduate </h3>
+		Computer Science:<br>
+			Offers advance topics in Computer Science to exsiting graduates in Computing or
+			a strongly-related discipline.
+			For more information <a href="http://www.rgu.ac.uk/computing/study-options/postgraduate/msc-computer-science">click here</a>
+		<br><br>
+		Computing - Software Technology with Network Management:<br>
+			Develop your skills in software development, database systems, network management and web programming.
+			For more information <a href="http://www.rgu.ac.uk/computing/study-options/postgraduate-taught-full-time/computing-software-technology-with-network-management"> click here</a>
+		<br><br>
+		Data Science:<br>
+			Data Science aims to produce speciallist in data science who have good understanding of the needs
+			of the industry.
+			For more information <a href="http://www.rgu.ac.uk/computing/study-options/postgraduate/masters-in-data-science">click here</a>
+
+</main>
+</div><!--End of Container-->
+
+<?php include_once("footer.php"); ?>
+
+</body>
+</html>
